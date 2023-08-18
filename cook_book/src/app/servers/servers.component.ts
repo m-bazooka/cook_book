ce67750f6b5df23b0ad6ff09cc9e21a1af10fa44
Mohamed Abdelhameed <mhmd.abdelhameed@gmail.com>
2023-08-18 15:08:35 +0200

    another upload

--- cook_book/src/app/servers/servers.component.ts
+++ cook_book/src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreation = 'No server was created!';
+  serverName ='';
+  serverCreated = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+    
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreation = 'Now the server ' + this.serverName + ' is created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  
 }
