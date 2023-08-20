e1c2000bd9239a0590f94ce458d21de154e69ebf
Mohamed Abdelhameed <mhmd.abdelhameed@gmail.com>
2023-08-20 21:17:54 +0200

    Adding new buttons and input fields

---
+++ cook_book/src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+// Long/normal version of creating a model
+// export class Ingredient {
+//     public name: string;
+//     public amount: number;
+
+//     constructor(name: string, amount: number) {
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+
+// Short version
+export class Ingredient {
+    constructor(public name: string, public amount: number) {
+    }
+}
--- cook_book/src/app/shopping-list/shopping-list.component.ts
+++ cook_book/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Garlic', 5),
+    new Ingredient('Onions', 2)
+  ];
 
+  consstructor() {}
 }
