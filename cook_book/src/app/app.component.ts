e588f6b4413d57c50d5393774a3d815b276ebb56
Mohamed Abdelhameed <mhmd.abdelhameed@gmail.com>
2023-08-22 20:33:30 +0200

    Adding recipe items to list and header functionality

--- cook_book/src/app/app.component.ts
+++ cook_book/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  usedSelection = 'recipe';
+  
+  onNavigate(selection: string) {
+    this.usedSelection = selection;
+  }
 }
--- cook_book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cook_book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 
@@ -7,12 +7,18 @@ import { Recipe } from './recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
+    new Recipe('Molokhia', 'The most famous dish that you should try.', 'https://www.saveur.com/uploads/2019/02/08/IKI3TYE6RT2PWSO5KLMHS4SBYI.jpg'),
     new Recipe('Koshari Masry', 'The most beloved dish all over Egypt.', 'https://scontent-ber1-1.xx.fbcdn.net/v/t39.30808-6/351160240_640956831406702_3863583054199081778_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cw6C5j7CgBMAX-7FF3H&_nc_ht=scontent-ber1-1.xx&oh=00_AfCaW3_XK-5RMEm2N1yRGAHEcwhY0nTPtl3_X5SxjNKSUg&oe=64E7819F')
   ];
 
   constructor() { }
 
   ngOnInit() { }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
